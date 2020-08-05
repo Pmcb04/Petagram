28a1af3793d3fd5bb9d72db8e4b66f0134840259
Pmcb04 <pmcarmona04@gmail.com>
2020-08-05 19:55:17 +0200

    Testing

---
+++ app/src/test/java/com/example/petagram/Testing.java
@@ -0,0 +1,22 @@
+package com.example.petagram;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class Testing {
+
+    @Test
+    public void test(){
+        System.out.println("Prueba superada con exito !!");
+    }
+    
+    public void main(String args[]){
+    
+    	test();
+    }
+    
+}
